21470fa76382781d8d5eed98cf94e1f0ab559526
rayxiao92 <rui.xiao@tufts.edu>
2014-07-04 22:44:59 -0400

    V1 - fixed Json, prompt

--- js/webcode.js
+++ js/webcode.js
@@ -1,7 +1,7 @@
 function genqrcode(){
     var table = document.getElementById('mytable');
     sum=0
-    jsonstring=""
+    jsonstring="["
 
     for (var r = 1, n = table.rows.length; r < n; r++) {
 //        for (var c = 1, m = table.rows[r].cells.length; c < m; c++) {
@@ -13,7 +13,7 @@ function genqrcode(){
             sum += quantity * price
 
             if (quantity != 0) {
-            	if ( jsonstring != "" ){
+            	if ( jsonstring != "[" ){
             		jsonstring += ","
             	}
 	            jsonstring += '{"name":"' + name +'","quantity":'+quantity+ ',"price":'+price +'}'
@@ -23,8 +23,9 @@ function genqrcode(){
             
 //        }
     }
+    jsonstring+=']'
     //jsonstring = encodeURIComponent(jsonstring)
-    alert(jsonstring)
+    //alert(jsonstring)
     // if(sum == 0) {
     // 	sum = "nothing"
     // }
@@ -32,10 +33,16 @@ function genqrcode(){
     // qr += jsonstring
     // qr += '&size=200x200" height = 200 width = 200>'
     prompt_array = JSON.parse(jsonstring);
+    prompt = "You had "
+    for (x in prompt_array) {
+    	prompt += prompt_array[x]["quantity"] + '个 ' + prompt_array[x]["name"] + ';'
+    }
+    prompt += "\nthe total is: " + sum + ". Please scan the QR code below to pay"
+    document.getElementById("confirmation").innerHTML = prompt
     //query = 'https://venmo.com/?txn=pay&recipients=ray-xiao&amount='+sum+'&note=nice food!! We had '+prompt_array["quantity"] + ' awesome ' + prompt_array["name"] +'!!Yummy!!&audience=private'
 
     query = 'https://venmo.com/?txn=pay&recipients=ray-xiao&amount='+sum+'&note=YummyAtRaysSteakHouse&audience=private'
-        console.log(query)
+    //    console.log(query)
     query = encodeURIComponent(query)
     qr = '<img src = "https://api.qrserver.com/v1/create-qr-code/?data='
     qr += query
